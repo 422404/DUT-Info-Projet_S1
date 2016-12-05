644a196086e37239e7eaba2d4b18d4d9e385bf31
422404 <elyanpoujol@gmail.com>
2016-12-06 00:24:33 +0100

    gros changements sur le menu et son interaction avec la page

--- script/script_menu.js
+++ script/script_menu.js
@@ -1,19 +1,43 @@
 
+
 // http://w3c.github.io/html/single-page.html#embedding-custom-non-visible-data-with-the-data-attributes
 function toggleMenu() {
   var menu = document.getElementById('menu');
   var bouton_menu = document.getElementById('menu_button');
-  
+  var page = document.getElementById('page');
+
   if (menu.dataset.state == "closed_menu") {
-      menu.dataset.state = "opened_menu";
-	  menu.style.width = "300px";
-	  menu.style.borderRight = "2px solid white";
-	  bouton_menu.style.backgroundImage = "url(\'../img/opened_menu_alpha_50px.png\')";
+    openMenu();
   }
   else if (menu.dataset.state == "opened_menu") {
-      menu.dataset.state = "closed_menu";
-	  menu.style.width = "0px";
-	  menu.style.borderRight = "0px solid white";
-	  bouton_menu.style.backgroundImage = "url(\'../img/closed_menu_alpha_50px.png\')";
+    closeMenu();
   }
+}
+
+function closeMenu() {
+  var menu = document.getElementById('menu');
+  var bouton_menu = document.getElementById('menu_button');
+  var page = document.getElementById('page');
+
+  menu.dataset.state = "closed_menu";
+  menu.style.width = "0px";
+  menu.style.borderRight = "0px solid #00FFFF";
+
+  bouton_menu.style.backgroundImage = "url(\'../img/closed_menu_alpha_50px.png\')";
+  
+  page.style.marginLeft = menu.style.width;
+}
+
+function openMenu() {
+  var menu = document.getElementById('menu');
+  var bouton_menu = document.getElementById('menu_button');
+  var page = document.getElementById('page');
+
+  menu.dataset.state = "opened_menu";
+  menu.style.width = "300px";
+  menu.style.borderRight = "2px solid #00FFFF";
+
+  bouton_menu.style.backgroundImage = "url(\'../img/opened_menu_alpha_50px.png\')";
+  
+  page.style.marginLeft = menu.style.width;
 }
