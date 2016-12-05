fc3e1259db64100e06d39784ccf0b6c70964124a
422404 <elyanpoujol@gmail.com>
2016-12-05 21:15:21 +0100

    icone avec couche alpha pour le menu

--- script/script_menu.js
+++ script/script_menu.js
@@ -6,10 +6,10 @@ function toggleMenu() {
   
   if (menu.className == "closed_menu") {
       menu.className = "opened_menu";
-	  bouton_menu.style.backgroundImage = "url(\'../img/opened_menu_50px.png\')";
+	  bouton_menu.style.backgroundImage = "url(\'../img/opened_menu_alpha_50px.png\')";
   }
   else if (menu.className == "opened_menu") {
       menu.className = "closed_menu";
-	  bouton_menu.style.backgroundImage = "url(\'../img/closed_menu_50px.png\')";
+	  bouton_menu.style.backgroundImage = "url(\'../img/closed_menu_alpha_50px.png\')";
   }
 }
