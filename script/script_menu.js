23a76bfada598891f836ab9f865ef882bc482523
422404 <elyanpoujol@gmail.com>
2016-12-05 19:38:43 +0100

    ajout de javascript pour le menu

---
+++ script/script_menu.js
@@ -0,0 +1,15 @@
+
+
+function toggleMenu() {
+  var menu = document.getElementById('menu');
+  var bouton_menu = document.getElementById('menu_button');
+  
+  if (menu.className == "closed_menu") {
+      menu.className = "opened_menu";
+	  bouton_menu.style.backgroundImage = "url(\'../img/opened_menu_50px.png\')";
+  }
+  else if (menu.className == "opened_menu") {
+      menu.className = "closed_menu";
+	  bouton_menu.style.backgroundImage = "url(\'../img/closed_menu_50px.png\')";
+  }
+}
