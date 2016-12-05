dee8dbff214ad14f590756455a8d68d87480027d
422404 <elyanpoujol@gmail.com>
2016-12-05 23:07:36 +0100

    ameloration du menu + test de plusieurs sections

--- script/script_menu.js
+++ script/script_menu.js
@@ -1,15 +1,19 @@
 
-
+// http://w3c.github.io/html/single-page.html#embedding-custom-non-visible-data-with-the-data-attributes
 function toggleMenu() {
   var menu = document.getElementById('menu');
   var bouton_menu = document.getElementById('menu_button');
   
-  if (menu.className == "closed_menu") {
-      menu.className = "opened_menu";
+  if (menu.dataset.state == "closed_menu") {
+      menu.dataset.state = "opened_menu";
+	  menu.style.width = "300px";
+	  menu.style.borderRight = "2px solid white";
 	  bouton_menu.style.backgroundImage = "url(\'../img/opened_menu_alpha_50px.png\')";
   }
-  else if (menu.className == "opened_menu") {
-      menu.className = "closed_menu";
+  else if (menu.dataset.state == "opened_menu") {
+      menu.dataset.state = "closed_menu";
+	  menu.style.width = "0px";
+	  menu.style.borderRight = "0px solid white";
 	  bouton_menu.style.backgroundImage = "url(\'../img/closed_menu_alpha_50px.png\')";
   }
 }
