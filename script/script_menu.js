11f13cf93119ba9a4a501e786b9394c26abb7da0
422404 <elyanpoujol@gmail.com>
2016-12-06 22:33:55 +0100

    Grosse maj

--- script/script_menu.js
+++ script/script_menu.js
@@ -3,8 +3,6 @@
 // http://w3c.github.io/html/single-page.html#embedding-custom-non-visible-data-with-the-data-attributes
 function toggleMenu() {
   var menu = document.getElementById('menu');
-  var bouton_menu = document.getElementById('menu_button');
-  var page = document.getElementById('page');
 
   if (menu.dataset.state == "closed_menu") {
     openMenu();
@@ -16,28 +14,24 @@ function toggleMenu() {
 
 function closeMenu() {
   var menu = document.getElementById('menu');
-  var bouton_menu = document.getElementById('menu_button');
+  var bouton_menu_icon = document.getElementById('menu_button_icon');
   var page = document.getElementById('page');
 
   menu.dataset.state = "closed_menu";
   menu.style.width = "0px";
-  menu.style.borderRight = "0px solid #00FFFF";
-
-  bouton_menu.style.backgroundImage = "url(\'../img/closed_menu_alpha_50px.png\')";
-  
+  //menu.style.borderRight = "none";
+  bouton_menu_icon.className = "fa fa-bars";
   page.style.marginLeft = menu.style.width;
 }
 
 function openMenu() {
   var menu = document.getElementById('menu');
-  var bouton_menu = document.getElementById('menu_button');
+  var bouton_menu_icon = document.getElementById('menu_button_icon');
   var page = document.getElementById('page');
 
   menu.dataset.state = "opened_menu";
   menu.style.width = "300px";
-  menu.style.borderRight = "2px solid #00FFFF";
-
-  bouton_menu.style.backgroundImage = "url(\'../img/opened_menu_alpha_50px.png\')";
-  
+  //menu.style.borderRight = "2px solid #00FFFF";
+  bouton_menu_icon.className = "fa fa-close";
   page.style.marginLeft = menu.style.width;
 }
