e2157efc09f6cc658e85bf63f17d2bf61f018a92
422404 <elyanpoujol@gmail.com>
2016-12-11 00:40:46 +0100

    Grosse MAJ, menu, menu ancres, style global, responsivite

---
+++ script/script_util.js
@@ -0,0 +1,19 @@
+function getTopOffset(elementId) {
+  var element = document.getElementById(elementId);
+  var offsets = element.getBoundingClientRect();
+  var top = offsets.top;
+  return top;
+}
+
+/*function debug_getTopOffset(elementId) {
+  var off = getTopOffset(elementId);
+  console.log(off);
+  return off;
+}*/
+
+function jumpToAnchor(anchorName) {
+  if (window.matchMedia("(max-width:600px)").matches && menu_anchors.dataset.state == "opened_menu") {
+    closeAnchorsMenu();
+  }
+  window.scrollTo(0, getTopOffset(anchorName) - 110); /* 110 == headerSize + 20 */
+}
