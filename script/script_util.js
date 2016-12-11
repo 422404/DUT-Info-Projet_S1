66d6cb07de0f8bc805a2b00f965069ea0ea859ad
422404 <elyanpoujol@gmail.com>
2016-12-11 18:39:00 +0100

    J'ai cassé le site, oups...

--- script/script_util.js
+++ script/script_util.js
@@ -5,11 +5,11 @@ function getTopOffset(elementId) {
   return top;
 }
 
-/*function debug_getTopOffset(elementId) {
+function debug_getTopOffset(elementId) {
   var off = getTopOffset(elementId);
   console.log(off);
   return off;
-}*/
+}
 
 function jumpToAnchor(anchorName) {
   if (window.matchMedia("(max-width:600px)").matches && menu_anchors.dataset.state == "opened_menu") {
